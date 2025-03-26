196a243044b4f3fd5a62236cc5ab3c872e53c780
Rafael <rafateimarques2@gmail.com>
2025-03-25 22:11:06 -0300

    okdsabn

---
+++ trote/js/scripts.js
@@ -0,0 +1,9 @@
+function calcular(){
+    let Mascote, Homenagem
+    Mascote = document.getElementById("Mascote").value
+    alert(Mascote)
+    Homenagem = document.getElementById("Homenagem")
+    alert(Homenagem)
+    let soma = Number(Mascote) + Number(Homenagem)
+    alert(soma)
+}
