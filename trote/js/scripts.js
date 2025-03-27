8c3ab68c4b2f52b5559a7e297684f39e054b24fe
Rafael <rafateimarques2@gmail.com>
2025-03-27 20:30:23 -0300

    adjsmiosa

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,9 +1,12 @@
 function calcular(){
-    let Mascote, Homenagem
-    Mascote = document.getElementById("Mascote").value
-    alert(Mascote)
-    Homenagem = document.getElementById("Homenagem")
-    alert(Homenagem)
-    let soma = Number(Mascote) + Number(Homenagem)
-    alert(soma)
+    let mascote, homenagem, leite, Kit_avulso, sangue, suplemento
+    mascote = document.getElementById("mascote").value
+    homenagem = document.getElementById("homenagem").value
+    leite = document.getElementById("leite").value
+    Kit_avulso =  document.getElementById("Kit_avulso").value
+    sangue = document.getElementById("sangue").value
+    suplemento = document.getElementById("suplemento").value
+    let soma = Number(mascote) + Number(homenagem) + Number(2 * leite) + Number(30 * Kit_avulso) + Number(15 * suplemento) + Number(20 * sangue)
+    //retorna o valor para o html
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
