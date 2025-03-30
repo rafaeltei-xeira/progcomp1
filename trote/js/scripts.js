9a5da1055301f3510c565fb593485e59b61274d4
rafaeltei-xeira <rafateimarques2@gmail.com>
2025-03-30 16:25:21 -0300

    Mudanças

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,12 +1,12 @@
 function calcular(){
-    let mascote, homenagem, leite, Kit_avulso, sangue, suplemento
+    let mascote, homenagem, leite, kit_avulso, sangue, suplementos
     mascote = document.getElementById("mascote").value
     homenagem = document.getElementById("homenagem").value
     leite = document.getElementById("leite").value
-    Kit_avulso =  document.getElementById("Kit_avulso").value
+    kit_avulso =  document.getElementById("kit_avulso").value
     sangue = document.getElementById("sangue").value
-    suplemento = document.getElementById("suplemento").value
-    let soma = Number(mascote) + Number(homenagem) + Number(2 * leite) + Number(30 * Kit_avulso) + Number(15 * suplemento) + Number(20 * sangue)
+    suplementos = document.getElementById("suplementos").value
+    let soma = Number(mascote) + Number(homenagem) + Number(2 * leite) + Number(30 * kit_avulso) + Number(15 * suplementos) + Number(20 * sangue)
     //retorna o valor para o html
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
